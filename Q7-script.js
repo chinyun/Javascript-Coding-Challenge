baa057813b3e4fb1f656578811d7ef72c01e8e92
chinyun <chenclaire17@gmail.com>
2019-01-08 16:27:32 +0800

    Add files via upload

---
+++ Q7-script.js
@@ -0,0 +1,25 @@
+// Question 7: Write the function sumInput() that:
+// Asks the user for values using prompt and stores the values in the array.
+// Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+// Calculates and returns the sum of array items.
+// P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+
+function sumInput() {
+	let storeArr = [];
+	while ( true ) {
+		let newValue = prompt('store a number in array?','');
+		
+		// check if user enter cancel, '' or non-numeric value?
+		if( newValue === null || newValue === '' || !isFinite(newValue)) break;
+		storeArr.push(+newValue);
+	}
+	alert( `You entered ${ storeArr }` );
+	
+	let sum = 0;
+	for ( let value of storeArr ){
+		sum += value;
+	}
+	return sum;
+}
+
+alert( `The sum of above numbers is ${sumInput()} ` ) ;
