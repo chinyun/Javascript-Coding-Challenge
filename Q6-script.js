c9b3dc1c47be83732f38dbec5f2f23f6040b6b54
chinyun <chenclaire17@gmail.com>
2019-01-16 21:48:13 +0800

    Update Q6-script.js

--- Q6-script.js
+++ Q6-script.js
@@ -1,4 +1,4 @@
-/* Question 7: Write the function sumInput() that:
+/* Question 6: Write the function sumInput() that:
 Asks the user for values using prompt and stores the values in the array.
 Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
 Calculates and returns the sum of array items.
