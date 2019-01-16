f3150838646a00c8a553d7a882e3fde639053c3c
chinyun <chenclaire17@gmail.com>
2019-01-16 21:28:14 +0800

    Update Q10-script.js

--- Q10-script.js
+++ Q10-script.js
@@ -1,4 +1,4 @@
-/*
+/* #1
 Filter unique array members.
 Create a function unique(arr) that should return an array with unique items of arr.
 */
@@ -6,8 +6,9 @@ let values = ["Hare", "Krishna", "Hare", "Krishna",
   "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ];
 
+// # solution 1-1
 function unique( arr ) {
-	let result =[];
+	let result =[ ];
 	for( let str of arr ){
 		if ( !result.includes( str ) ) result.push( str );
 	}
@@ -17,7 +18,7 @@ function unique( arr ) {
 alert( unique( values ) ); // Hare, Krishna, :-O
 
 
-// another solution by using 'set'
+// # solution 1-2: using 'set'
 function unique( arr ) {
 	let set = Array.from( new Set( arr ) ) ;
 	return set;
@@ -26,15 +27,13 @@ function unique( arr ) {
 alert(  unique( values ) ); // Hare, Krishna, :-O
 
 
-/*
+/* #2
 Filter Anagrams. Anagrams are words have the same number of same letters, but in different order.
 array = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"]
 Create a function that clean(array) should returns "nap,teachers,ear" or "PAN,cheaters,era".
 */
 
 let array = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-
-
 
 function clean ( arr ) {
 	let map = new Map( );
