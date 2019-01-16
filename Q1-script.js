8565bd933c383ec834d67dea4cd1f159f4ac64f6
chinyun <chenclaire17@gmail.com>
2019-01-16 21:45:42 +0800

    Update Q1-script.js

--- Q1-script.js
+++ Q1-script.js
@@ -1,8 +1,10 @@
-// Question 1: Clean the room function: 
-// given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20],
-// make a function that organizes these into individual array that is ordered. 
-// For example answer(ArrayFromAbove) should return: 
-// [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. 
+/*
+Question 1: Clean the room function: 
+given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20],
+make a function that organizes these into individual array that is ordered. 
+For example answer(ArrayFromAbove) should return: 
+[[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. 
+*/
 
 const arr = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20];
 ArrayFromAbove();
